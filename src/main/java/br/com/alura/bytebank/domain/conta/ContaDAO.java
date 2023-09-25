7aae00fbc4fe1fb9f2ec618e2ea3732f95d16024
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-09-25 15:22:47 -0300

    First commit - criando ContaDAO, ConnecetionFactory

---
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -0,0 +1,68 @@
+package br.com.alura.bytebank.domain.conta;
+
+import br.com.alura.bytebank.domain.cliente.Cliente;
+import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContaDAO {
+
+    private Connection connection;
+
+    public ContaDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void salvar(DadosAberturaConta dadosDaConta) {
+        var cliente = new Cliente(dadosDaConta.dadosCliente());
+        var conta = new Conta(dadosDaConta.numero(), cliente);
+
+        String sql = "INSERT INTO conta(numero, saldo, cliente_nome, cliente_cpf, cliente_email)" + "VALUES(?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1,conta.getNumero());
+            preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
+            preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
+            preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
+            preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
+
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Set<Conta> listar() {
+        Set<Conta> contas = new HashSet<>();
+
+        String sql = "SELECT * FROM conta";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Integer numero = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+                contas.add(new Conta(numero, cliente));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return contas;
+    }
+}
